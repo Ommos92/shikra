82a6906bc0cbb012be8e701aaf810d4c617e24d5
Andrew El Kommos <an963984@ucf.edu>
2024-03-31 10:26:08 -0400

    Update the step before loading the image since the code keeps adding an image folder path to the image path.

--- mllm/dataset/utils/io.py
+++ mllm/dataset/utils/io.py
@@ -21,6 +21,8 @@ def read_img_general(img_path):
         # noinspection PyUnresolvedReferences
         return Image.fromarray(cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB))
     else:
+        # Remove the directory Image from the path - /datasets/VG/images/VG_100K/2364674.jpg
+        img_path = img_path.replace('/images/', '/')
         return Image.open(img_path).convert('RGB')
 
 
